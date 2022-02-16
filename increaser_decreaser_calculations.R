b17b4aafc13690a41cca65c26260fa729ea3ad04
NGMack <mack.nathan@gmail.com>
2022-02-16 10:16:50 -0800

    Update increaser_decreaser_calculations.R
    I do not know what etiquette  is when  collaborating on code, so I left yours alone and added a section of mine which filters the metaData and uses it as a lookup table for the summary stats.  I created variables for each of the sites based upon their SWAMP codes (above, below) and made a pivot table to view the results.

--- increaser_decreaser_calculations.R
+++ increaser_decreaser_calculations.R
@@ -28,3 +28,35 @@ results$Suppl1_OE %>%
 
 # Traits
 
+
+# Nathan's addition 02162022-------------------------------------------------------
+report <- CSCI(bugs, stations)
+
+md <- loadMetaData() %>% 
+  select(FinalID, SAFIT1__OTU_a, FunctionalFeedingGroup) %>% 
+  filter(!FunctionalFeedingGroup == '')
+
+data <- report$Suppl1_OE %>% 
+  mutate(year = year(mdy(str_sub(.$SampleID, start = 11)))) %>% 
+  mutate(location = str_sub(.$StationCode, start = -1)) %>% #location codes a A=above, B=below, R=river, from SWAMP code
+  mutate(stream = str_sub(.$StationCode, start = 7, end = 7)) %>%  
+#Rafi's code below
+  group_by(location,OTU) %>% #any kind of grouping can be added here
+  summarise(Si = sum(CaptureProb),
+            N = SampleID %>% unique %>% length(),
+            Fe = Si/N,
+            Fo = sum(MeanObserved>0)/N,
+            Df=Fo-Fe) %>%
+  ungroup() %>% 
+  mutate(Df=Fo-Fe,
+         Type= case_when(Df> 0 ~'increasers', #possibly change these thresholds
+                         Df< 0 ~'decreasers',
+                         Df == 0 ~ 'neutral',
+                         T~ 'error')) %>% 
+  mutate(FFG = md$FunctionalFeedingGroup[match(.$OTU, md$SAFIT1__OTU_a)]) %>% 
+  arrange (Df)
+#give a pivot table of which ffg are increasing and decreasing by sample location
+#change group_by() for different variable
+data_ffg <- data %>% group_by(location, Type, FFG) %>% summarise(n = n()) %>% 
+  pivot_wider(names_from = Type, values_from = n) %>% 
+  select(location, FFG, increasers, decreasers, everything())
